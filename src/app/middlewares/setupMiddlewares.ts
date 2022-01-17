8fe45668f3beebfe8ec3fd5afd42567ce9b514fb
reviakin <reviakindi@gmail.com>
2022-01-17 19:00:04 +0200

    MODULE-7

--- src/app/middlewares/setupMiddlewares.ts
+++ src/app/middlewares/setupMiddlewares.ts
@@ -1,9 +1,12 @@
 import { Express, json } from "express";
+import helmet from "helmet";
+
 import { logRequest } from ".";
 
 const setupMiddlewares = (app: Express) => {
   app.use(json());
   app.use(logRequest);
+  app.use(helmet());
 };
 
 export { setupMiddlewares };
